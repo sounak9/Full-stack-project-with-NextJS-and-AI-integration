6421e1fbb88d161554915310a4b979b8e3779728
sounak9 <sounakbera9@gmail.com>
2025-02-22 19:11:22 +0530

    basic-file-structure

---
+++ my-app/src/model/User.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: { type: String, required: true },
+    createdAt: {
+        type: Date, required: true,
+        default: Date.now
+     },
+});
+
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isAcceptingMessage: boolean;
+    messages: Message[]; 
+
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Please provide a username'],
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide a email'],
+        trim: true,
+        unique: true, 
+        match: [/.+\@.+\..+/, 'Please provide a valid email']
+     },
+});
